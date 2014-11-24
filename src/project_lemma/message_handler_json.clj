a36c309f3e71b58884f3a1954b3e8b685cdc4a50
Jon Wettersten <jwettersten@ideo.com>
2014-11-24 11:26:08 -0600

    refactored payload parsing, still in progress

--- src/project_lemma/message_handler_json.clj
+++ src/project_lemma/message_handler_json.clj
@@ -6,10 +6,35 @@
 
 ; json message handler
 ; pass this into server to read and format messages
+; need to add exception handling here
+; validate length
 (defn read
   [reader]
-  (json/read reader))
+  (.readLine reader))
 
+; message parser notes:
+; if (!message.equals("")) {
+;    Event event = MessageParser.parseEvent(message);
+;    filter.handle(event);
+; }
+;
+; 000042["event","guest1","topic1",[1,2,"potato"]]
+(defn payload-match?
+  [msg]
+  (try
+    (let [msg-length-count (Integer/parseInt (subs msg 0 6))
+         msg-payload-data (count (subs msg 6))]
+      (if (= msg-length-count msg-payload-data) true false))
+    (catch Exception e
+      (print e))))
+
+(defn parse-payload
+  [msg]
+  (try
+    (let [msg-payload-data (subs msg 6)]
+      (if (payload-match? msg) true false))
+    (catch Exception e
+      (print e))))
 
 (defn helloJSON [name]
   (let [json-reader (json/read-str name :key-fn keyword)]
--- test/project_lemma/message_handler_json_test.clj
+++ test/project_lemma/message_handler_json_test.clj
@@ -1,6 +1,13 @@
 (ns project-lemma.message-handler-json-test
   (:require [clojure.test :refer :all]
+            [clojure.data.json :as json]
             [project-lemma.message-handler-json :refer :all]))
+
+(deftest test-msg-payload-count-pass
+         (testing "comparing byte count with actual payload size"
+                 (let [test-json-reader (str "000042[\"event\",\"guest1\",\"topic1\",[1,2,\"potato\"]]")]
+                       (assert (= true (parse-payload test-json-reader))))))
+
 
 (deftest test-helloJSON
   (testing "helloJSON"
