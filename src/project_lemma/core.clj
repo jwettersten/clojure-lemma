8ce38dd2cc8ddf7817d03a8f5e7dd3a294c2a2e0
Jon Wettersten <jwettersten@ideo.com>
2014-12-18 14:40:16 -0600

    removed noam discovery broadcast ip and port values, now replaced by constants in lemma maodule

--- src/project_lemma/core.clj
+++ src/project_lemma/core.clj
@@ -25,9 +25,8 @@
   ; Setup and register lemma
   ; Send test event messages for 10 seconds
   ;
-  ; remove passing in broadcast ip 255.255.255.255 and 1030 - shame on me
   ; locating, registering all needs to be encapsulated in lemma.
-  (lemma/locate-noam "255.255.255.255" 1030 "guest1" "clojure-noam"
+  (lemma/locate-noam "guest1" "clojure-noam"
                      (fn [{polo :type, noam-name :room-name, noam-port :port, noam-ip :ip}]
                        (let [register (lemma/init "guest1" noam-ip noam-port {"topic1" topic1-handler "topic2" topic2-handler "topic3" topic3-handler})]
                          (let [send-event (:send-event register)]
